698e3a27b31c67cedf7eb3e2103cc9e8649f3a74
Matthias Eggert <matthias.eggert@qytera.de>
2025-03-11 11:02:48 +0100

    spring boot init

---
+++ src/main/groovy/de/besessener/cloudthrash/CloudthrashApplication.groovy
@@ -0,0 +1,13 @@
+package de.besessener.cloudthrash
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CloudthrashApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CloudthrashApplication, args)
+	}
+
+}
---
+++ src/test/groovy/de/besessener/cloudthrash/CloudthrashApplicationTests.groovy
@@ -0,0 +1,13 @@
+package de.besessener.cloudthrash
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudthrashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
