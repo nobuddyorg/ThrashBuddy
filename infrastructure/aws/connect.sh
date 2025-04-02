505f9afdd1fd8ba2a6c817039de9915a5528992c
Matthias Eggert <matthias.eggert@qytera.de>
2025-04-02 15:50:55 +0200

    remove port forwarding completely again

--- infrastructure/aws/connect.sh
+++ infrastructure/aws/connect.sh
@@ -5,10 +5,4 @@ pushd $(dirname $0) > /dev/null
 . ./env.sh
 eksctl utils write-kubeconfig --cluster ${CLUSTER_NAME} && kubectl config use-context $(kubectl config get-contexts -o name | grep ${CLUSTER_NAME})
 
-kubectl port-forward deployment/ingress-nginx-controller 80:80 >/dev/null 2>&1 &
-
-echo ""
-echo -e "\e[1;34mVisit CloudThrash at: http://localhost:80/\e[0m"
-echo ""
-
 popd > /dev/null
