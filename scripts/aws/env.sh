82831ac4850b1345898308b0b9240e44469481b6
Matthias Eggert <dem.matze@gmail.com>
2025-06-24 17:09:15 +0200

    pick correct default region

--- scripts/aws/env.sh
+++ scripts/aws/env.sh
@@ -9,7 +9,7 @@ export EKS_CLUSTER_NAME="${APP_NAME}-${EKS_ENVIRONMENT_NAME}"
 
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 export AWS_DEFAULT_REGION=$(aws configure get region)
-export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-e-central-1}
+export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-eu-central-1}
 
 export ECR_REPOSITORY="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
 
